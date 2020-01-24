4ae3fe1054be6385c7c4e71667e374157f6c9642
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-01-24 08:22:39 -0800

    set up Redux files

---
+++ smurfs/src/actions/smurfActions.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const FETCHING_SMURFS_START = 'FETCHING_SMURFS_START';
+export const FETCHING_SMURFS_SUCCESS = 'FETCHING_SMURFS_SUCCESS';
+export const FETCHING_SMURFS_FAIL = 'FETCHING_SMURFS_FAIL';
+
+export const fetchSmurfs = () => dispatch => {
+  dispatch({ type: FETCHING_SMURFS_START });
+  axios.get('http://localhost:3333/smurfs')
+  .then(res => {
+    console.dir(res.data);
+    dispatch({ type: FETCHING_SMURFS_SUCCESS, payload: res.data })
+  })
+  .catch(err => {
+    dispatch({ type: FETCHING_SMURFS_FAIL, payload: err.response })
+  })
+};
---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  isFetching: false,
+  items: null,
+  error: ''
+};
+
+const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default smurfReducer;
